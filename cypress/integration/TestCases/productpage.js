aace8f514897911746962fd9bbad746ff7a7f4ef
Arif Ali <alyaref555@gmail.com>
2023-09-13 12:12:23 +0500

    Indentation
    Tutorial by Adil

--- cypress/integration/TestCases/productpage.js
+++ cypress/integration/TestCases/productpage.js
@@ -2,9 +2,8 @@
 
 describe ('This is the test suite for automating website scripts.', ()=> {
 
-it('Here, we will first access the website and then proceed to the product page.', ()=>{
-    cy.visit('https://www.seesight-tours.com/')
-
-})
+    it('Here, we will first access the website and then proceed to the product page.', ()=>{
+        cy.visit('https://www.seesight-tours.com/')
+    })
     
 })
